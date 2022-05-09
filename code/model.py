f3ab1ed4a36e580bc03392f5fafec178e7c7317f
Luke West <lukewest73@gmail.com>
2022-05-08 22:42:13 -0400

    add bias regularization, stop sharing regularizers

--- code/model.py
+++ code/model.py
@@ -5,7 +5,11 @@ from tensorflow.keras import layers
 import hyperparameters as hp
 
 act = tf.nn.leaky_relu
-l2 = tf.keras.regularizers.L2(l2=1e-3)
+def bias_l2():
+    return tf.keras.regularizers.l2(l2=1e-3)
+
+def kernel_l2():
+    return tf.keras.regularizers.l2(l2=1e-3)
 
 # avoid regularization/dropout at transition points, and on embeddings
 # avoid activation near end, since colors aren't binary
@@ -16,26 +20,26 @@ l2 = tf.keras.regularizers.L2(l2=1e-3)
 ########## Image convolution (shrinking)
 imgs_inputs = keras.Input(shape=(hp.img_size, hp.img_size, 3))
 
-conv = layers.Conv2D(8, 3, activation=act, kernel_regularizer=l2, bias_initializer='glorot_uniform')(imgs_inputs)
+conv = layers.Conv2D(8, 3, activation=act, kernel_regularizer=kernel_l2(), bias_regularizer=bias_l2(), bias_initializer='glorot_uniform')(imgs_inputs)
 conv = layers.LayerNormalization(axis=[1,2])(conv)
 # (126, 126, 8)
 
-conv = layers.Conv2D(16, 3, activation=act, strides=2, kernel_regularizer=l2, bias_initializer='glorot_uniform')(conv)
+conv = layers.Conv2D(16, 3, activation=act, strides=2, kernel_regularizer=kernel_l2(), bias_regularizer=bias_l2(), bias_initializer='glorot_uniform')(conv)
 conv = layers.LayerNormalization(axis=[1,2])(conv)
 conv = layers.AlphaDropout(0.05)(conv)
 # (62, 62, 16)
 
-conv = layers.Conv2D(32, 3, activation=act, strides=2, kernel_regularizer=l2, bias_initializer='glorot_uniform')(conv)
+conv = layers.Conv2D(32, 3, activation=act, strides=2, kernel_regularizer=kernel_l2(), bias_regularizer=bias_l2(), bias_initializer='glorot_uniform')(conv)
 conv = layers.LayerNormalization(axis=[1,2])(conv)
 conv = layers.AlphaDropout(0.05)(conv)
 # (30, 30, 32)
 
-conv = layers.Conv2D(64, 3, activation=act, strides=2, kernel_regularizer=l2, bias_initializer='glorot_uniform')(conv)
+conv = layers.Conv2D(64, 3, activation=act, strides=2, kernel_regularizer=kernel_l2(), bias_regularizer=bias_l2(), bias_initializer='glorot_uniform')(conv)
 conv = layers.LayerNormalization(axis=[1,2])(conv)
 conv = layers.AlphaDropout(0.05)(conv)
 # (14, 14, 64)
 
-conv = layers.Conv2D(16, 3, activation=act, kernel_regularizer=l2, bias_initializer='glorot_uniform')(conv)
+conv = layers.Conv2D(16, 3, activation=act, kernel_regularizer=kernel_l2(), bias_regularizer=bias_l2(), bias_initializer='glorot_uniform')(conv)
 conv = layers.LayerNormalization(axis=[1,2])(conv)
 # (12, 12, 16)
 
@@ -44,7 +48,7 @@ conv = layers.LayerNormalization(axis=[1,2])(conv)
 
 ########## Image flattening
 flat = layers.Flatten()(conv)
-flat = layers.Dense(1024, activation=act, kernel_regularizer=l2, bias_initializer='glorot_uniform')(flat)
+flat = layers.Dense(1024, activation=act, kernel_regularizer=kernel_l2(), bias_regularizer=bias_l2(), bias_initializer='glorot_uniform')(flat)
 flat = layers.LayerNormalization(axis=[1])(flat)
 
 
@@ -69,9 +73,9 @@ combo = layers.Dense(1024, activation=act, bias_initializer='glorot_uniform')(co
 combo = layers.LayerNormalization(axis=[1])(combo)
 combo = layers.Dense(1024, activation=act, bias_initializer='glorot_uniform')(combo)
 combo = layers.LayerNormalization(axis=[1])(combo)
-combo = layers.Dense(1024, activation=act, kernel_regularizer=l2, bias_initializer='glorot_uniform')(combo)
+combo = layers.Dense(1024, activation=act, kernel_regularizer=kernel_l2(), bias_regularizer=bias_l2(), bias_initializer='glorot_uniform')(combo)
 combo = layers.LayerNormalization(axis=[1])(combo)
-combo = layers.Dense(1024, activation=act, kernel_regularizer=l2, bias_initializer='glorot_uniform')(combo)
+combo = layers.Dense(1024, activation=act, kernel_regularizer=kernel_l2(), bias_regularizer=bias_l2(), bias_initializer='glorot_uniform')(combo)
 combo = layers.LayerNormalization(axis=[1])(combo)
 
 
@@ -79,13 +83,13 @@ combo = layers.LayerNormalization(axis=[1])(combo)
 
 ########## Deconvolution (growing)
 deconv = layers.Reshape((8, 8, 16))(combo)
-deconv = layers.Conv2DTranspose(16, 3, strides=2, activation=act, kernel_regularizer=l2, bias_initializer='glorot_uniform')(deconv)
+deconv = layers.Conv2DTranspose(16, 3, strides=2, activation=act, kernel_regularizer=kernel_l2(), bias_regularizer=bias_l2(), bias_initializer='glorot_uniform')(deconv)
 deconv = layers.LayerNormalization(axis=[1, 2])(deconv)
-deconv = layers.Conv2DTranspose(16, 3, strides=2, activation=act, kernel_regularizer=l2, bias_initializer='glorot_uniform')(deconv)
+deconv = layers.Conv2DTranspose(16, 3, strides=2, activation=act, kernel_regularizer=kernel_l2(), bias_regularizer=bias_l2(), bias_initializer='glorot_uniform')(deconv)
 deconv = layers.LayerNormalization(axis=[1, 2])(deconv)
-deconv = layers.Conv2DTranspose(16, 3, strides=2, activation=act, kernel_regularizer=l2, bias_initializer='glorot_uniform')(deconv)
+deconv = layers.Conv2DTranspose(16, 3, strides=2, activation=act, kernel_regularizer=kernel_l2(), bias_regularizer=bias_l2(), bias_initializer='glorot_uniform')(deconv)
 deconv = layers.LayerNormalization(axis=[1, 2])(deconv)
-deconv = layers.Conv2DTranspose(16, 3, strides=2, kernel_regularizer=l2, bias_initializer='glorot_uniform')(deconv)
+deconv = layers.Conv2DTranspose(16, 3, strides=2, kernel_regularizer=kernel_l2(), bias_regularizer=bias_l2(), bias_initializer='glorot_uniform')(deconv)
 deconv = layers.LayerNormalization(axis=[1, 2])(deconv)
 deconv = layers.Conv2D(3, 16, bias_initializer='glorot_uniform')(deconv)
 
